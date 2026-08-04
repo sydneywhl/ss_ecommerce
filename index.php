dddcb2ec14c2d20b1bd321f7378a0e6c4562d021
Sean <seanfelsonfelix@gmail.com>
2026-08-04 23:52:13 +0800

    created the index page with search bar, cart and user icon

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <title>Document</title>
+</head>
+<body class="index-body">
+    <header class="index-header">
+        <div class="logo">
+            <h1>SS E-commerce</h1>
+        </div>
+        
+        <form class="search-bar" action="" method="GET">
+            <input type="text" name="search" placeholder="Search products...">
+            <button type="submit"><i class="fas fa-search"></i></button>
+        </form>
+
+        <div class="header-icons">
+            <a href="shopping_cart.php"><i class="fas fa-shopping-cart"></i></a>
+            <a href="log_in.php"><i class="fas fa-user"></i></a>
+        </div>
+    </header>
+
+    <main>
+        <section>
+        
+        </section>
+    </main>
+
+</body>
+</html> 
